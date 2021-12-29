72728f1aebc87406537758fd9a71999e93191796
Frumentius Daneswara <frumentius44@gmail.com>
2021-12-29 11:29:30 +0700

    first commit

---
+++ get_nama.php
@@ -0,0 +1,21 @@
+<?php
+
+// $kel1_api = "http://192.168.18.67:8001";
+
+function get_nama($id_participant){
+	// echo $kel1_api;
+    $kel1_api = "http://192.168.18.67:8001";
+	$url = $kel1_api."/items/customer?fields=customer_id,customer_name&filter[customer_id]=".$id_participant;
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($curl);
+    $hasil = json_decode($response, true);
+    if(empty($hasil["data"])){
+    	return "Kamu";
+    } else {
+	    $nama = $hasil["data"][0]["customer_name"];
+	    return $nama;
+	}
+}
+?>
